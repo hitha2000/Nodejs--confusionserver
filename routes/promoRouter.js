cc9ce7ae1a3e1a758d42167d0b78bc9eb4c955e8
Hithaishi S <hithaishigowda73328@gmail.com>
2020-08-11 18:29:40 +0530

    Passport Jwt

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Promotions = require('../models/promotions');
+const authenticate = require('../authenticate');
 
 const promoRouter = express.Router();
 
@@ -16,7 +17,7 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post( (req,res,next) => {
+.post( authenticate.verifyUser, (req,res,next) => {
     Promotions.create(req.body)
     .then((promo) => {
         console.log('Promotion created ', promo);
@@ -27,12 +28,12 @@ promoRouter.route('/')
     .catch((err) => next(err));
  })
  
- .put((req,res,next) => {
+ .put( authenticate.verifyUser, (req,res,next) => {
      res.statusCode = 403;
      res.end('PUT operation not supported on /promotions');
  })
  
- .delete((req,res,next) => {
+ .delete( authenticate.verifyUser, (req,res,next) => {
     Promotions.remove({})
     .then((resp) => {
      res.statusCode=200;
@@ -55,12 +56,12 @@ promoRouter.route('/:promoId')
      .catch((err) => next(err));
  })
  
- .post( (req,res,next) => {
+ .post( authenticate.verifyUser, (req,res,next) => {
      res.statusCode = 403;
      res.end('POST operation not supported on /promotions/' + req.params.promoId);
  })
  
- .put( (req,res,next) => {
+ .put( authenticate.verifyUser, (req,res,next) => {
    Promotions.findByIdAndUpdate(req.params.promoId, {
        $set: req.body
    }, {new : true})
@@ -72,7 +73,7 @@ promoRouter.route('/:promoId')
      .catch((err) => next(err));
  })
  
- .delete((req,res,next) => {
+ .delete( authenticate.verifyUser, (req,res,next) => {
    Promotions.findByIdAndRemove(req.params.promoId)
    .then((resp) => {
      res.statusCode=200;
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+//const { authenticate } = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -34,28 +36,12 @@ router.post('/signup',function(req,res,next)
   
 });
 
-/*router.post('/signup', (req, res, next) => {
-  User.register(new User({username: req.body.username}), 
-    req.body.password, (err, user) => {
-    if(err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      passport.authenticate('local')(req, res, () => {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({success: true, status: 'Registration Successful!'});
-      });
-    }
-  });
-});*/
-
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
+  var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  res.json({success: true, status: 'You are successfully logged in!'});
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 router.get('/logout', (req, res, next) => {
